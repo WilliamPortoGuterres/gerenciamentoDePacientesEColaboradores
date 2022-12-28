f58a5afd1a8b09e78e59f1f784134fbddcb7c888
will4879 <will4879@gmail.com>
2022-12-28 13:34:40 -0300

    alteraçoes não significativas

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,4 +1,27 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿<script>
+    // Get the form element
+    var form = document.getElementById('formCadastro');
 
-// Write your JavaScript code.
+    // Add a submit event listener to the form
+    form.addEventListener('submit', function(event) {
+        // Prevent the default form submission behavior
+        event.preventDefault();
+
+    // Get the form data
+    var data = new FormData(form);
+
+    // Make an AJAX request to the server
+    var xhr = new XMLHttpRequest();
+    xhr.open('POST', '/CadastroController/Create', true);
+    xhr.onload = function() {
+        if (xhr.status === 200) {
+        // The form submission was successful
+        // Do something here, like showing a success message or redirecting to another page
+    } else {
+        // There was an error with the form submission
+        // Do something here, like showing an error message
+    }
+      };
+    xhr.send(data);
+    });
+</script>
